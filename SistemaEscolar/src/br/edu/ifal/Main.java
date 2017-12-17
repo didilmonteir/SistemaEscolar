b71ff45e75c8f2970e2fd200512684e94530fbd2
Jose Matias <matiasjose547@gmail.com>
2017-12-17 12:34:50 -0300

    atualizacão diaria

--- SistemaEscolar/src/br/edu/ifal/Main.java
+++ SistemaEscolar/src/br/edu/ifal/Main.java
@@ -1,10 +1,11 @@
 package br.edu.ifal;
 
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
 	}
 
 }
--- SistemaEscolar/src/br/edu/ifal/Professor.java
+++ SistemaEscolar/src/br/edu/ifal/Professor.java
@@ -5,37 +5,40 @@ public class Professor {
 	private String nome;
 	private String formacao;
 	private String cpf;
-	
-		
+
 	public Professor() {
 		super();
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
-		if(nome != null) {
-		this.nome = nome;
+		if (nome != null) {
+			this.nome = nome;
 		}
 	}
+
 	public String getFormacao() {
 		return formacao;
 	}
+
 	public void setFormacao(String formacao) {
-		if(formacao != null) {
-		this.formacao = formacao;
+		if (formacao != null) {
+			this.formacao = formacao;
 		}
-		
+
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		if (cpf != null) {
 			this.cpf = cpf;
 		}
 	}
-	
-	
+
 }
