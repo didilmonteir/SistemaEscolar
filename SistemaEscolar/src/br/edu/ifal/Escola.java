68d5c69ae25dd9f1c49db51e169c173c84d4fa59
didilmonteir <didilmonteiro70@gmail.com>
2017-12-17 22:27:49 -0300

    Enviei a classe Escola

---
+++ SistemaEscolar/src/br/edu/ifal/Escola.java
@@ -0,0 +1,41 @@
+package br.edu.ifal;
+
+import java.util.List;
+
+public class Escola {
+	
+	private String nome;
+	private List<Curso> cursos;
+	private List<Professor> professores;
+		
+	public Escola() {
+		super();
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		if (nome != null) {
+			this.nome = nome;
+		}
+	}
+	
+	public List<Curso> getCursos() {
+		return cursos;
+	}
+	public void setCursos(List<Curso> cursos) {
+		if (cursos != null) {
+			this.cursos = cursos;
+		}
+	}
+	public List<Professor> getProfessores() {
+		return professores;
+	}
+	public void setProfessores(List<Professor> professores) {
+		if (professores != null) {
+			this.professores = professores;
+		}
+	}
+	
+}
