eb0785ab41ff2386559b941e7c2deb019d8d9553
Rebeca Alexandre <rebecaalexandre6@gmail.com>
2017-12-22 09:52:45 -0300

    criacao da classe disciplina

---
+++ SistemaEscolar/src/br/edu/ifal/Disciplina.java
@@ -0,0 +1,65 @@
+package br.edu.ifal;
+
+import java.util.List;
+
+public class Disciplina {
+	
+	private String nome;
+	private String professor;
+	private List<Alunos> Alunos;
+	
+	public Disciplina() {
+		super();
+		this.nome = "";
+		this.professor = "";
+		this.alunos = "";
+		
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getProfessor() {
+		return professor;
+	}
+
+	public void setProfessor(String professor) {
+		this.professor = professor;
+	}
+
+	public String getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(String alunos) {
+		this.alunos = alunos;
+	}
+	
+	// adicionar e remover da lista
+			public void addDiciplinas(Disciplina disciplina) {
+				if (disciplina!=null) {
+					this.disciplina.add(disciplina);
+				}	
+			}
+			public void removeDiciplina(Disciplina disciplina ) {
+				this.disciplina.remove(disciplina);
+			}
+			
+			
+			public void addDic(Alunos alunos) {
+				if (alunos!=null) {
+					this.alunos.add(alunos);
+				}	
+			}
+			public void removeDiciplina(Alunos alunos ) {
+				this.alunos.remove(alunos);
+			}
+
+	}
+
