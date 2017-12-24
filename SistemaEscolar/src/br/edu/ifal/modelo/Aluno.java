9c7cf8e299e22bdac4f2a4d670981dab13767e5c
matias <matiasjose547@gmail.com>
2017-12-24 02:20:55 -0300

    A adicionado notacões Entity e modificado para Calendar o tipo do atributo dataNascimento da classe Aluno

--- SistemaEscolar/src/br/edu/ifal/modelo/Aluno.java
+++ SistemaEscolar/src/br/edu/ifal/modelo/Aluno.java
@@ -1,19 +1,33 @@
 package br.edu.ifal.modelo;
 
+import java.util.Calendar;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "aluno")
 public class Aluno {
 
-	private String nome;
+	@Id
 	private String matricula;
+	@Column
+	private String nome;
+	@Column
 	private String cpf;
-	private String dataNascimento;
+	@Temporal(TemporalType.DATE)
+	@Column(name = "data_nascimento", nullable = true)
+	private Calendar dataNascimento;
+	@Column
 	private String nomePai;
+	@Column
 	private String nomeMae;
 
-	public Aluno() { //Contrutor padrão
-		
+	public Aluno() { // Contrutor padrão
+
 	}
-	//Construtor de sobrecarga com recebimento de parametros
-	public Aluno(String nome, String matricula, String cpf, String dataNascimento, String nomePai, String nomeMae) {
+
+	// Construtor de sobrecarga com recebimento de parametros
+	public Aluno(String nome, String matricula, String cpf, Calendar dataNascimento, String nomePai, String nomeMae) {
 		super();
 		this.nome = nome;
 		this.matricula = matricula;
@@ -23,61 +37,61 @@ public class Aluno {
 		this.nomeMae = nomeMae;
 	}
 
-	public String getNome() { //retorna o nome do aluno
+	public String getNome() { // retorna o nome do aluno
 		return nome;
 	}
 
-	public void setNome(String nome) { //altera o nome do aluno
+	public void setNome(String nome) { // altera o nome do aluno
 		if (nome != null) {
 			this.nome = nome;
 		}
 	}
 
-	public String getMatricula() { //retorna a matricula
+	public String getMatricula() { // retorna a matricula
 		return matricula;
 	}
 
-	public void setMatricula(String matricula) { //altera a matricula
+	public void setMatricula(String matricula) { // altera a matricula
 		if (nome != null) {
 			this.matricula = matricula;
 		}
 	}
 
-	public String getCpf() { //retorna o CPF
+	public String getCpf() { // retorna o CPF
 		return cpf;
 	}
 
-	public void setCpf(String cpf) { //altera o CPF
+	public void setCpf(String cpf) { // altera o CPF
 		if (cpf != null) {
 			this.cpf = cpf;
 		}
 	}
 
-	public String getDataNascimento() { //retorna a data de nascimento
+	public Calendar getDataNascimento() { // retorna a data de nascimento
 		return dataNascimento;
 	}
 
-	public void setDataNascimento(String dataNascimento) { //altera a data de nascimento
+	public void setDataNascimento(Calendar dataNascimento) { // altera a data de nascimento
 		if (dataNascimento != null) {
 			this.dataNascimento = dataNascimento;
 		}
 	}
 
-	public String getNomePai() { //retorna o nome do pai
+	public String getNomePai() { // retorna o nome do pai
 		return nomePai;
 	}
 
-	public void setNomePai(String nomePai) { //altera o nome do pai
+	public void setNomePai(String nomePai) { // altera o nome do pai
 		if (nomePai != null) {
 			this.nomePai = nomePai;
 		}
 	}
 
-	public String getNomeMae() { //retorna o nome do mae
+	public String getNomeMae() { // retorna o nome do mae
 		return nomeMae;
 	}
 
-	public void setNomeMae(String nomeMae) { //altera o nome do mae
+	public void setNomeMae(String nomeMae) { // altera o nome do mae
 		if (nomeMae != null) {
 			this.nomeMae = nomeMae;
 		}
