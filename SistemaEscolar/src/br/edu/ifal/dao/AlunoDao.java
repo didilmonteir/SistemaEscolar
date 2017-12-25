30223d15040393cdd8a04f650d3eb702656eeefa
matias <matiasjose547@gmail.com>
2017-12-25 00:06:48 -0300

    Criado metodo adicionarAluno na classe AlunoDao

--- SistemaEscolar/src/br/edu/ifal/dao/AlunoDao.java
+++ SistemaEscolar/src/br/edu/ifal/dao/AlunoDao.java
@@ -9,6 +9,11 @@ public class AlunoDao {
 	EntityManagerFactory factory = Persistence.createEntityManagerFactory("Aluno");
 	EntityManager manager = factory.createEntityManager();
 
+	public void adicionarAluno(Aluno a) {
+		manager.getTransaction().begin();
+		manager.persist(a);
+		manager.getTransaction().commit();
+	}
 	
 
 }
