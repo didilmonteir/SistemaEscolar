2d66414f659c9317deb37562ae3cfec392def0b2
Edvaldo de Almeida Monteiro <34115558+didilmonteir@users.noreply.github.com>
2018-02-02 11:09:59 -0300

    Edição da classe ProfessorDao para adicionar métodos atualizar,remover,adicionar e exibir

--- SistemaEscolar/src/br/edu/ifal/dao/ProfessorDao.java
+++ SistemaEscolar/src/br/edu/ifal/dao/ProfessorDao.java
@@ -4,32 +4,43 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import br.edu.ifal.modelo.Aluno;
 import br.edu.ifal.modelo.Professor;
 
 public class ProfessorDao {
-	public static void main(String[] args) {
+	
 		Professor professor = new Professor();
-		professor.setNome("Tia Priscylla");
-		professor.setFormacao("Mestre em ciência da computação");
-		professor.setCpf("111222333");
 		
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("professor");
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("Professor");
 		EntityManager manager = factory.createEntityManager();
 		
-		manager.getTransaction().begin();
-		manager.persist(professor);
-		manager.getTransaction().commit();
-		
-		System.out.println("Cpf do professo: "+ professor.getCpf());
-		
-		Professor encontrado = manager.find(Professor.class, 1L);
-		System.out.println(encontrado.getNome());
-		
-		 manager.getTransaction().begin();
-         manager.remove(encontrado);
-         manager.getTransaction().commit();
-		
-         manager.close();
-		
-	}
+		public void adicionarProfessor(Professor p) { // adiciona novo objeto ao banco
+			manager.getTransaction().begin();
+			manager.persist(p);
+			manager.getTransaction().commit();
+			manager.close();
+		}
+
+		public Professor exibirProfessor(String cpf) { // visualiza objeto do banco
+			manager.getTransaction().begin();
+			Professor p = manager.find(Professor.class, cpf);
+			manager.getTransaction().commit();
+			manager.close();
+			return p;
+		}
+
+		public void atualizarProfessor(Professor p) { // atualiza as informacoes do objeto no banco
+			manager.getTransaction().begin();
+			manager.merge(p);
+			manager.getTransaction().commit();
+			manager.close();
+		}
+
+		public void removerRemover(String cpf) { // remove aluno do banco
+			manager.getTransaction().begin();
+			Aluno a = manager.find(Aluno.class, cpf);
+			manager.remove(a);
+			manager.getTransaction().commit();
+			manager.close();
+		}
 }
