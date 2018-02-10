14e8ca57c2cc612efaf3ce7209b21ee43f969b0a
José Matias da Silva <34017323+TheMatias@users.noreply.github.com>
2018-02-10 09:47:15 -0300

    Bugs da classe DisciplinaDao eliminados

--- SistemaEscolar/src/br/edu/ifal/dao/DiciplinaDao.java
+++ SistemaEscolar/src/br/edu/ifal/dao/DiciplinaDao.java
@@ -3,50 +3,41 @@ package br.edu.ifal.dao;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
-import br.edu.ifal.modelo.Aluno;
+import br.edu.ifal.modelo.Disciplina;
 
 public class DiciplinaDao {
 
-/*
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("Diciplina");
-		EntityManager manager = factory.createEntityManager();
+	EntityManagerFactory factory = Persistence.createEntityManagerFactory("default");
+	EntityManager manager = factory.createEntityManager();
 
-		public void adicionarDiciplina(Diciplina d) { // adiciona novo objeto ao banco
-			manager.getTransaction().begin();
-			manager.persist(a);
-			manager.getTransaction().commit();
-			manager.close();
-		}
-
-		public Aluno visualizarDiciplina(String matricula) { // visualiza objeto do banco
-			manager.getTransaction().begin();
-			Aluno a = manager.find(Diciplina.class, matricula);
-			manager.getTransaction().commit();
-			manager.close();
-			return a;
-		}
-
-		public void atualizarAluno(Aluno a) { // atualiza as informacoes do objeto no banco
-			manager.getTransaction().begin();
-			manager.merge(a);
-			manager.getTransaction().commit();
-			manager.close();
-		}
-
-		public void removerAluno(String matricula) { // remove aluno do banco
-			manager.getTransaction().begin();
-			Aluno a = manager.find(Aluno.class, matricula);
-			manager.remove(a);
-			manager.getTransaction().commit();
-			manager.close();
-		}*/
-
-		/*
-		 * public List<Aluno> listarAlunos() { // metodo responsavel de listar nome e
-		 * matricula manager.getTransaction().begin(); List<Aluno> alunos =
-		 * manager.createQuery("select nome,matricula from aluno;").getResultList();
-		 * manager.getTransaction().commit(); return alunos; }
-		 */
-
+	public void adicionarDiciplina(Disciplina d) { // adiciona novo objeto ao banco
+		manager.getTransaction().begin();
+		manager.persist(d);
+		manager.getTransaction().commit();
+		manager.close();
 	}
+
+	public Disciplina visualizarDisciplina(String nome) { // visualiza objeto do banco
+		manager.getTransaction().begin();
+		Disciplina d = manager.find(Disciplina.class, nome);
+		manager.getTransaction().commit();
+		manager.close();
+		return d;
+	}
+
+	public void atualizarAluno(Disciplina d) { // atualiza as informacoes do objeto no banco
+		manager.getTransaction().begin();
+		manager.merge(d);
+		manager.getTransaction().commit();
+		manager.close();
+	}
+
+	public void removerDisciplina(String nome) { // remove aluno do banco
+		manager.getTransaction().begin();
+		Disciplina d = manager.find(Disciplina.class, nome);
+		manager.remove(d);
+		manager.getTransaction().commit();
+		manager.close();
+	}
+
+}
