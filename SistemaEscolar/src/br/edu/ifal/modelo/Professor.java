f10cdd9d83907a2c92ebbc8ea190d6728fffe898
didilmonteir <didilmonteiro70@gmail.com>
2018-04-11 00:30:39 -0300

    Adicionei os getters e setters de TipoProfessor na classe Professor

--- SistemaEscolar/src/br/edu/ifal/modelo/Professor.java
+++ SistemaEscolar/src/br/edu/ifal/modelo/Professor.java
@@ -2,6 +2,8 @@ package br.edu.ifal.modelo;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -18,6 +20,7 @@ public class Professor {
 	@Column(length = 80, nullable=false, name = "formacao")
 	private String formacao;
 	@Column(name = "tipo_professor", nullable = false)
+	@Enumerated(EnumType.STRING)
 	private TipoProfessor tipoProfessor;
 
 	public enum TipoProfessor{
@@ -58,6 +61,14 @@ public class Professor {
 		if (cpf != null) {
 			this.cpf = cpf;
 		}
+	}
+
+	public TipoProfessor getTipoProfessor() {
+		return tipoProfessor;
+	}
+
+	public void setTipoProfessor(TipoProfessor tipoProfessor) {
+		this.tipoProfessor = tipoProfessor;
 	}
 
 
