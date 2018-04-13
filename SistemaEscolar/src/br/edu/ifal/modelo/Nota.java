afaa317387b32decba46480299ef9edd0d3a5440
didilmonteir <didilmonteiro70@gmail.com>
2018-04-12 23:12:35 -0300

    Alterei a anotação do atributo valor na Classe Nota

--- SistemaEscolar/src/br/edu/ifal/modelo/Nota.java
+++ SistemaEscolar/src/br/edu/ifal/modelo/Nota.java
@@ -2,21 +2,27 @@ package br.edu.ifal.modelo;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "nota")
 
 public class Nota {
 	@Id
+	@GeneratedValue(generator = "inc")
+	@GenericGenerator(name = "inc", strategy = "increment")
 	private int id;
+	
 	@ManyToOne
 	private Aluno aluno;
 	@ManyToOne
 	private Disciplina disciplina;
-	@Column
+	@Column(length = 2, scale = 2, nullable = true, name ="valor")
 	private double valor;
 
 	public Nota(int id, Aluno aluno, Disciplina disciplina, double valor) {
