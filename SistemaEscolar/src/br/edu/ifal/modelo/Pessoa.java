b8b84a1aa4ebab3bd1432dd138993a9a7ba793b3
didilmonteir <didilmonteiro70@gmail.com>
2018-04-13 00:05:45 -0300

    Foram adicionadas mais anotações na classe Pessoa, para identificar os atributos como da classe Super

--- SistemaEscolar/src/br/edu/ifal/modelo/Pessoa.java
+++ SistemaEscolar/src/br/edu/ifal/modelo/Pessoa.java
@@ -1,10 +1,19 @@
 package br.edu.ifal.modelo;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "pessoa")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo")
 public abstract class  Pessoa {
 	
-	@Column(length = 200, nullable = false, name = "nome_aluno")
+	@Column(length = 200, nullable = false, name = "aluno")
 	private String nome;
 
 	@Column(length = 200, nullable = false, name = "cpf")
