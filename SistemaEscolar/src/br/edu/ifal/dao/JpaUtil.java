96aa6a261f13a20788de31039dd73a1c45c90352
Matias <matiasjose547@gmail.com>
2018-04-17 18:30:26 -0300

    Retirado import's desnecessarios

--- SistemaEscolar/src/br/edu/ifal/dao/JpaUtil.java
+++ SistemaEscolar/src/br/edu/ifal/dao/JpaUtil.java
@@ -1,16 +1,13 @@
 package br.edu.ifal.dao;
 
-import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.hamcrest.Factory;
-
 public class JpaUtil {
-	
+
 	private static EntityManagerFactory factory;
-	
+
 	static {
 		factory = Persistence.createEntityManagerFactory("default");
 	}
@@ -18,9 +15,9 @@ public class JpaUtil {
 	public static EntityManager getEntityManager() {
 		return factory.createEntityManager();
 	}
-	
+
 	public static void close() {
 		factory.close();
 	}
-	
+
 }
