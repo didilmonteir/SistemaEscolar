ee5e588b019b616c684ab9f78d5fbcba78c08e81
Matias <matiasjose547@gmail.com>
2018-04-25 01:18:55 -0300

    Modificado forma de atribuir valor do objeto aluno na classe de Test GerenciadorDeNotasTest

--- SistemaEscolar/test/br/edu/ifal/servico/GerenciadorDeNotaTest.java
+++ SistemaEscolar/test/br/edu/ifal/servico/GerenciadorDeNotaTest.java
@@ -11,33 +11,29 @@ import br.edu.ifal.modelo.Disciplina;
 public class GerenciadorDeNotaTest {
 
 	private Aluno aluno;
-	private Disciplina disciplina; 
+	private Disciplina disciplina;
 	private GerenciadorDeNotas gerenciadorDeNotas;
-	
+
 	@Before
 	public void criacaoDasEntidades() {
-		aluno = new Aluno();
-		aluno.setNome("Rebeca");
-		aluno.setCpf("000.001.123-50");
-		
+		aluno = new Aluno(1, "Rebeca", "00000112350");
+
 		disciplina = new Disciplina();
 		disciplina.setNome("POO");
-		
+
 		gerenciadorDeNotas = new GerenciadorDeNotas();
-		
+
 	}
-	
+
 	@Test
 	public void naoDeveSetarNotaMenorQueZero() {
-		
+
 		boolean resultado = gerenciadorDeNotas.setarNotas(12, aluno, disciplina);
-		
+
 		boolean resultadoEsperado = false;
-		
+
 		assertEquals(resultadoEsperado, resultado);
-		
+
 	}
-	
-	
-	
+
 }
