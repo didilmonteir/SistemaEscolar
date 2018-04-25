1379148e3b9b7f557d89aa8c061d0ef8385d909b
Matias <matiasjose547@gmail.com>
2018-04-25 00:48:50 -0300

    Adicionando motodos Getters e Setters da Classe Boletim

--- SistemaEscolar/src/br/edu/ifal/modelo/Boletim.java
+++ SistemaEscolar/src/br/edu/ifal/modelo/Boletim.java
@@ -11,17 +11,63 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
+
 @Entity
-@Table(name= "boletim")
+@Table(name = "boletim")
 public class Boletim {
 	@Id
 	@GeneratedValue(generator = "inc")
-	@GenericGenerator(name = "inc", strategy = "increment")	
-	@Column(length = 10, nullable = false, name = "id_boletim")
-	private int id;	
+	@GenericGenerator(name = "inc", strategy = "increment")
+	@Column(nullable = false, name = "id_boletim")
+	private Integer id;
 	@OneToMany
 	private Set<Disciplina> disciplinas;
-	@Column(length = 4, name = "notas", nullable = true)
-	private List<Byte> notas;
-	
+	@Column(length = 4, name = "notas")
+	private List<Nota> notas;
+
+	public Boletim(Set<Disciplina> disciplinas, List<Nota> notas) {
+		this.disciplinas = disciplinas;
+		this.notas = notas;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Set<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
+
+	public void setDisciplinas(Set<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
+	}
+
+	public void removerDisciplina(Disciplina d) {
+		disciplinas.remove(d);
+	}
+
+	public void adicionarDisciplina(Disciplina d) {
+		disciplinas.add(d);
+	}
+
+	public List<Nota> getNotas() {
+		return notas;
+	}
+
+	public void setNotas(List<Nota> notas) {
+		this.notas = notas;
+	}
+
+	public void removerNota(Nota n) {
+		notas.remove(n);
+	}
+
+	public void adicionarNota(Nota n) {
+		notas.add(n);
+	}
+
 }
