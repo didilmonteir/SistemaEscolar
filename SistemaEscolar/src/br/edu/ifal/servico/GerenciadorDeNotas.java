80f6c4ac16cdd3f5d7093bda36d067d39c1c2a6f
Matias <matiasjose547@gmail.com>
2018-04-25 01:14:43 -0300

    modificado operador lógico && para || na classe de serviço GerenciadorDeNotas

--- SistemaEscolar/src/br/edu/ifal/servico/GerenciadorDeNotas.java
+++ SistemaEscolar/src/br/edu/ifal/servico/GerenciadorDeNotas.java
@@ -9,7 +9,7 @@ public class GerenciadorDeNotas {
 	private Nota nota;
 
 	public boolean setarNotas(double valorNota, Aluno aluno, Disciplina disciplina) {
-		if (valorNota < 0 && valorNota > 10 && aluno == null && disciplina == null) {
+		if (valorNota < 0 || valorNota > 10 || aluno == null || disciplina == null) {
 			return false;
 		} else {
 			nota = new Nota(10, aluno, disciplina, valorNota);
